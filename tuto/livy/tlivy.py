d97e832e2297ec4987530c3a48316913d6b3a1b7
Christian Arnault <arnault@lal.in2p3.fr>
2019-03-22 16:54:00 +0100

    Improve the python tuto for livy

--- tuto/livy/tlivy.py
+++ tuto/livy/tlivy.py
@@ -5,16 +5,48 @@ import pprint
 
 host = "http://vm-75222.lal.in2p3.fr:21111"
 
-headers = {'Content-Type': 'application/json'}
+def livy_get_sessions():
+    ids=[]
+    r = requests.get(host + '/sessions')
+    answer = r.json()
+    for session in answer['sessions']:
+        ids.append(session['id'])
 
-data = {'kind': 'pyspark'}
-r = requests.post(host + '/sessions', data=json.dumps(data), headers=headers)
-print(r.json())
+    return ids
 
-data = {'code': '1 + 1'}
-r = requests.post(host + '/statements', data=json.dumps(data), headers=headers)
-# pprint.pprint(r.json())
+def livy_create_session():
+    headers = {'Content-Type': 'application/json'}
+    data = {'kind': 'pyspark'}
+    r = requests.post(host + '/sessions', data=json.dumps(data), headers=headers)
+    answer = r.json()
+    id = int(answer['id'])
+    return int(answer['id'])
 
-requests.delete(host + '/session/1', headers=headers)
+def livy_delete_session(id):
+    r = requests.delete(host + '/sessions/{}'.format(id))
 
 
+def main():
+    print('Show all session ids')
+    print(' - '.join([str(id) for id in livy_get_sessions()]))
+
+    print('Create a new session')
+    id = livy_create_session()
+    print(id)
+
+    print('Delete it')
+    print(livy_delete_session(id))
+
+    print('Show all session ids')
+    print(' - '.join([str(id) for id in livy_get_sessions()]))
+
+    """
+    data = {'code': '1 + 1'}
+    r = requests.post(host + '/statements', data=json.dumps(data), headers=headers)
+    # pprint.pprint(r.json())
+    
+    """
+
+
+if __name__ == "__main__":
+    main()
