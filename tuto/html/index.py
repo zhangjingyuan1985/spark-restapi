dd47f91eac7d1c85ae38fa882be3a86c4344d6b2
Christian Arnault <arnault@lal.in2p3.fr>
2019-03-29 16:42:15 +0100

    tuto/html> First completion of the full livy cycle

--- tuto/html/index.py
+++ tuto/html/index.py
@@ -49,61 +49,92 @@ https://pylivy.readthedocs.io/en/latest/index.web
 
 # Initialize post variables
 class Variable:
-    def __init__(self, name):
+    def __init__(self, name, type="int"):
         self.name = name
-        self.value = None
+        self.type = type
+        self.reset()
 
     def read(self):
         try:
-            self.value = int(form.getvalue(self.name))
+            if self.type == "int":
+                self.value = int(form.getvalue(self.name))
+            else:
+                value = form.getvalue(self.name)
+                if value is None:
+                    value = ""
+                self.value = value
+                pass
         except:
-            self.value = -1
+            self.reset()
+        pass
 
     def to_form(self):
         out = """<input type="hidden" name="{}" value="{}" />""".format(self.name, self.value)
         return out
 
     def debug(self):
-        out = "<br> {} = {}\n".format(self.name, self.value)
+        out = " {} = {}\n".format(self.name, self.value)
         return out
 
     def reset(self):
-        self.value = -1
+        if self.type == "int":
+            self.value = -1
+        else:
+            self.value = ""
+        pass
 
     def set(self, value):
-        try:
-            self.value = int(value)
-        except:
-            self.value = -1
+        if self.type == "int":
+            try:
+                self.value = int(value)
+            except:
+                self.value = -1
+        else:
+            self.value = value
 
     def is_set(self):
-        try:
-            if self.value >= 0:
-                return True
-        except:
-            pass
+        if self.type == "int":
+            try:
+                if self.value >= 0:
+                    return True
+            except:
+                pass
+        else:
+            try:
+                if len(self.value) > 0:
+                    return True
+            except:
+                pass
 
         return False
 
     def incr(self):
-        self.value += 1
+        if self.type == "int":
+            self.value += 1
 
     def above(self, threshold):
-        try:
-            if self.value > threshold:
-                return True
-        except:
-            pass
+        if self.type == "int":
+            try:
+                if self.value > threshold:
+                    return True
+            except:
+                pass
 
         return False
 
 
 class VariableSet:
-    def __init__(self, names):
+    def __init__(self, names, str_names):
         self.base = dict()
 
+        type = "int"
+
         for name in names:
-            self.base[name] = Variable(name)
+            if name in str_names:
+                type = "str"
+            else:
+                type = "int"
+            self.base[name] = Variable(name, type)
 
     def variable(self, name):
         return self.base[name]
@@ -131,22 +162,36 @@ LIVY_URL = "http://vm-75222.lal.in2p3.fr:21111"
 form = cgi.FieldStorage()
 print("Content-type: text/html; charset=utf-8\n")
 
-# init data
-variables = VariableSet(["start", "simul", "change_simul", "session", "waiting_session", "waiting_statement", "statement"])
+client = LivyClient(LIVY_URL)
 
-start = variables.variable("start")
-simul = variables.variable("simul")
-change_simul = variables.variable("change_simul")
-session = variables.variable("session")
-waiting_session = variables.variable("waiting_session")
-waiting_statement = variables.variable("waiting_statement")
-statement = variables.variable("statement")
+# init data
+variables = VariableSet(["start",
+                         "simul",
+                         "change_simul",
+                         "livy_session",
+                         "waiting_session",
+                         "waiting_statement",
+                         "livy_statement",
+                         "new_statement",
+                         "kill_session",
+                         "result"], ["new_statement", "result"])
+
+start = variables.base["start"]
+simul = variables.base["simul"]
+change_simul = variables.base["change_simul"]
+livy_session = variables.base["livy_session"]
+waiting_session = variables.base["waiting_session"]
+waiting_statement = variables.base["waiting_statement"]
+livy_statement = variables.base["livy_statement"]
+kill_session = variables.base["kill_session"]
+new_statement = variables.base["new_statement"]
+result = variables.base["result"]
 
 variables.read()
 
-if start.is_set():
+if not start.is_set():
     simul.set(1)
-    start.reset()
+    start.set(1)
 
 
 # ======================================================
@@ -217,22 +262,23 @@ html += """
     <form action="/index.py" method="post" name="operations">
         """
 
-if waiting_session.above(5):
-    print("<br> session is now idle")
-    waiting_session.reset()
-    waiting_statement.reset()
-    statement.reset()
-    session.set(1)
+if simul.is_set():
+    if waiting_session.above(5):
+        print("<br> session is now idle")
+        waiting_session.reset()
+        waiting_statement.reset()
+        livy_statement.reset()
+        livy_session.set(1)
 
-if waiting_statement.above(5):
-    print("<br> statement just finished")
-    waiting_session.reset()
-    waiting_statement.reset()
-    statement.incr()
+    if waiting_statement.above(5):
+        print("<br> statement just finished")
+        waiting_session.reset()
+        waiting_statement.reset()
+        livy_statement.incr()
 
 # debugging
-print("<br>")
-print("Keys = [", ",".join(form.keys()), "]")
+# print("<br>")
+# print("Keys = [", ",".join(form.keys()), "]")
 print(variables.debug())
 
 """
@@ -242,26 +288,52 @@ Command interface
 - start statement & wait for completion
 """
 
-if session.is_set():
+if kill_session.is_set():
+    id = livy_session.value
+    try:
+        client.delete_session(id)
+    except:
+        print("error killing session ", id)
+
+    livy_session.reset()
+    waiting_session.reset()
+    kill_session.reset()
+
+if livy_session.is_set():
     # statement management
     if not waiting_statement.is_set():
-        html += """<br>session is idle start a staement<br>"""
+        html += """<br>session is idle: we may start a statement<br>"""
         waiting_statement.set(0)
         html += variables.to_form()
-        html += """Enter a Spark statement <input type="text" name="new_statement" value="code" />"""
+        html += """
+        Enter a Spark statement 
+        <input type="text" name="new_statement" value="{}" /> 
+        <input type="text" name="result" value="{}" />
+        <button type="submit">Run</button>        
+        """.format(new_statement.value, result.value)
     else:
-        html += """<br>session is idle waiting a staement to complete<br>"""
+        html += """<br>session is idle, we do wait a statement to complete<br>"""
         waiting_statement.incr()
+        id = livy_session.value
+        s = client.get_session(id)
+        if not livy_statement.is_set():
+            st = client.create_statement(s.session_id, new_statement.value)
+            livy_statement.set(st.statement_id)
+        else:
+            st = client.get_statement(s.session_id, livy_statement.value)
+            if st.state == StatementState.AVAILABLE:
+                waiting_statement.reset()
+                result.set(st.output.text)
+                print("<br>", result.value)
+                livy_statement.reset()
+
         html += variables.to_form()
-        html += """<button type="submit">waiting statement</button>"""
+        html += """<button type="submit">waiting statement to complete</button>"""
 else:
     # session management
     if not waiting_session.is_set():
         html += """<br>No session<br>"""
         waiting_session.set(0)
-
-        if not simul.is_set():
-            pass
 
         print(waiting_session.debug())
 
@@ -269,12 +341,47 @@ else:
         html += variables.to_form()
         html += """<button type="submit">Open a session</button>"""
     else:
+        # we have requested a new session thus waiting_session is set
+
+        if simul.is_set():
+            waiting_session.incr()
+        else:
+
+            if not livy_session.is_set():
+                print("Create a session ")
+                s = client.create_session(SessionKind.PYSPARK)
+                print("<br> session {} <br>".format(s.session_id))
+                livy_session.set(s.session_id)
+
+            # we test if the session is already idle
+            id = livy_session.value
+            s = client.get_session(id)
+            if s.state == SessionState.IDLE:
+                print("<br> session is now idle")
+                waiting_session.reset()
+                waiting_statement.reset()
+                livy_statement.reset()
+                new_statement.reset()
+
         html += """<br>Waiting session to become idle<br>"""
-        waiting_session.incr()
         html += variables.to_form()
         html += """<button type="submit">waiting session</button>"""
 
 html += """</form>"""
+
+if livy_session.is_set():
+    html += """
+    <form action="/index.py" method="post" name="operations">"""
+
+    kill_session.set(1)
+    html += variables.to_form()
+    html += """
+         <button type="submit">Delete the session</button>
+    </form>
+    """
+
+
+
 
 html += """
       </div>
@@ -291,15 +398,6 @@ print(html)
 
 
 """
-print("Create a session ")
-session = client.create_session(SessionKind.PYSPARK)
-print("=> session ", session.session_id)
-
-print("... wait until idle ...")
-while True:
-    session = client.get_session(s.session_id)
-    if session.state == SessionState.IDLE:
-        break
 
 code = form.getvalue("statement")
 if code != "":
